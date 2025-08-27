dec1adfc6069f43df269fd45ec19bac95de74aef
Gehendraraj Pyakurel <gehendraraj.pyakurel@sifal.deerwalk.edu.np>
2025-08-27 23:42:30 +0545

    Sync Changes

---
+++ src/main.jsx
@@ -0,0 +1,10 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import.meta.glob('./styles/*.css', { eager: true })
+import App from './App.jsx'
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+)
